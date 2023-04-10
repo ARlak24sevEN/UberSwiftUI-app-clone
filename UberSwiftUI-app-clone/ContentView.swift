001cdcade7b09f0a67a069799d48c0d5cef5f973
Arlak <arlak.a@orcsoft.co.th>
2023-04-10 21:49:05 +0700

    Initial Commit

---
+++ UberSwiftUI-app-clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UberSwiftUI-app-clone
+//
+//  Created by Arlak Abdulloh on 10/4/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UberSwiftUI-app-clone/UberSwiftUI_app_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUI_app_cloneApp.swift
+//  UberSwiftUI-app-clone
+//
+//  Created by Arlak Abdulloh on 10/4/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUI_app_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
