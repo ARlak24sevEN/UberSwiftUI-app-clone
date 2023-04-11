0b59af020d8227117a5f48bf0cc45b50a96e0eb9
Arlak <arlak.a@orcsoft.co.th>
2023-04-11 12:43:52 +0700

    add ui search and action button

--- UberSwiftUI-app-clone/Core/Home/Views/HomeView.swift
+++ UberSwiftUI-app-clone/Core/Home/Views/HomeView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        UberMapViewRepresentable()
-            .ignoresSafeArea()
+        ZStack(alignment: .top) {
+            UberMapViewRepresentable()
+                .ignoresSafeArea()
+            LocationSearchActivationView()
+            .padding(.top, 72)
+            
+            MapViewActionButton()
+                .padding(.leading)
+        }
     }
 }
 
